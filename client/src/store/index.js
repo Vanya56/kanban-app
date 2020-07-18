45b45c4808cf080a4afaa51be206c4b9f039cb96
Vanya Levchenko <ivanlevchenko56@gmail.com>
2020-07-17 23:29:20 -0600

    Updated feathers.js store.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import feathersVuex from './feathersVuex';
+
+import localAuth from './localAuth';
+import board from './board';
+
+const {
+  service,
+  auth,
+  FeathersVuex,
+} = feathersVuex;
+
 Vue.use(Vuex);
+Vue.use(FeathersVuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  plugins: [
+  auth({
+    userService: 'users',
+  })
+],
 });
